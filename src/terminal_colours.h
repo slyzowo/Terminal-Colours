24a3da322af6428c609fd12e5555147eb5036610
slyzOwO <lilithhimes@gmail.com>
2025-12-06 17:16:40 -0600

    Update terminal_colours.h

--- src/terminal_colours.h
+++ src/terminal_colours.h
@@ -1,6 +1,6 @@
 #ifndef TERMINAL_COLOURS_H
 #define TERMINAL_COLOURS_H
 
-void coloured_print(int attribute, int backgroundColour, int textColour, char text[]);
+void coloured_print(int red, int green, int blue, char text[]);
 
 #endif /* TERMINAL_COLOURS_H */

3de1b7c37aa40b8b3b5e98362808b2ad1eb9124a
slyzOwO <lilithhimes@gmail.com>
2026-03-25 22:22:31 -0500

    Added colored_printf() and made "colour" -> "color"

--- src/terminal_colours.h
+++ src/terminal_colours.h
@@ -7,11 +7,17 @@
   extern "C" {
 #endif
 
-  void coloured_print(int red ,int green, int blue, const char* text);
+void colored_print(int red ,int green, int blue, const char* text);
+void colored_printf(int red, int green, int blue, const char *fmt, ...);
 
-  void error_print(const char* text);
+void error_print(const char* text);
+void error_printf(const char* text);
 
-  void warning_print(const char* text);
+void warning_print(const char* text);
+void warning_printf(const char* text);
+
+void success_print(const char* text);
+void success_printf(const char* text);
 
 #ifdef __cplusplus
   }

71d2b090c139ac36d886e01033d8d5f3f9f28e9b
slyzOwO <lilithhimes@gmail.com>
2026-07-08 21:55:30 -0500

    abaababa

--- src/terminal_colors.c
+++ src/terminal_colors.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void cprint(int red ,int green, int blue, const char* text){
+void colored_print(int red ,int green, int blue, const char* text){
   printf("\033[38;2;%d;%d;%dm%s\033[0m", red, green, blue, text);
 }
 
-void cprintf(int red, int green, int blue, const char *fmt, ...){
+void colored_printf(int red, int green, int blue, const char *format, ...){
   printf("\033[38;2;%d;%d;%dm", red, green, blue);
   va_list args;
-  va_start(args, fmt);
-  vprintf(fmt, args);
+  va_start(args, format);
+  vprintf(format, args);
   va_end(args);
   printf("\033[0m");
 }
